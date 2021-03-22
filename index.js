bfc9a173c9a49148e3bf32fab275c3b128bcc7d1
Dan Horwood <dpj@sent.com>
2021-03-22 14:05:18 +0800

    4.2

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const middleware = require('./utils/middleware')
+const app = require('./app')
 const logger = require('./utils/logger')
-const Blog = require('./models/blog')
 
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
 
-app.use('/api/blogs', blogsRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
--- models/blog.js
+++ models/blog.js
@@ -1,15 +1,4 @@
 const mongoose = require('mongoose')
-const logger = require('../utils/logger')
-const config = require('../utils/config')
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
-  })
 
 const blogSchema = new mongoose.Schema({
   title: String,
