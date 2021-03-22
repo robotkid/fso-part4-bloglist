aa6dd214cddae08a1802c60066e4665a27ef695b
Dan Horwood <2223214+robotkid@users.noreply.github.com>
2021-03-23 06:09:31 +0800

    4.8

---
+++ tests/blogs_api.test.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+const { blogs: initialBlogs } = require('./testData')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  for await (let b of initialBlogs) {
+    const blogObject = new Blog(b)
+    await blogObject.save()
+  }
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are six blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(6)
+})
+
+test('the first blog is about React patterns', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0].title).toBe('React patterns')
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
