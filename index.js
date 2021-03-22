e2990c0f7cc07dd27c38710538b65851814dd513
Dan Horwood <dpj@sent.com>
2021-03-22 13:54:37 +0800

    refactor routes and middleware

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 const config = require('./utils/config')
 const express = require('express')
+const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const Blog = require('./models/blog')
 
-const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 const PORT = config.PORT
 app.listen(PORT, () => {
